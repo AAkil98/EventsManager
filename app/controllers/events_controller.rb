5c30f809695c567bbe17fd2774380eead81be1b7
Abderrahim Akil <aakilabderr22@gmail.com>
2024-08-27 17:29:33 +0100

    View of Events created//

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,48 @@
 class EventsController < ApplicationController
+  before_action :set_event, only:[:show, :edit, :update, :destroy]
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path(@path)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, status: :see_other
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:category, :description, :starting_day, :ending_day)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
