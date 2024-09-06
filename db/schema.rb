5c81b47ccf721574b4e4d69b94e43a89a8a33a33
Wedade Jerjoub <wedadejerjoub@gmail.com>
2024-09-06 17:00:39 +0100

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_27_140603) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_02_112620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_27_140603) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,70 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+Service.destroy_all
+
+services = [
+
+  {
+    title: "Night Clubs",
+    description: "Experience the ultimate nightlife at our exclusive night clubs. Enjoy world-class DJs and vibrant atmospheres. Unforgettable nights await you.",
+    image_url: "nightclubs.jpg"
+  },
+  {
+    title: "Hotel Booking",
+    description: "Secure the best deals with our hotel booking services. Enjoy luxurious accommodations and exceptional amenities. Make your stay unforgettable with us.",
+    image_url: "hotelbooking.jpg"
+  },
+  {
+    title: "Wedding Styling",
+    description: "Transform your special day with our expert wedding styling. Enjoy personalized designs and elegant themes. Create unforgettable memories with us.",
+    image_url: "weddingstyling.jpg"
+  },
+  {
+    title: "Evening Programs",
+    description: "Enhance your event with our curated evening programs. Enjoy engaging entertainment and unique experiences. Make your evenings memorable with us.",
+    image_url: "eveningprograms.jpg"
+  },
+  {
+    title: "On-Site Management",
+    description: "Ensure flawless events with our on-site management. Enjoy seamless coordination and professional oversight. Let us handle the details for your peace of mind.",
+    image_url: "onsitemanagement.jpg"
+  },
+  {
+    title: "Sweets and Desserts",
+    description: "Delight your guests with our exquisite sweets and desserts. Enjoy a variety of delicious treats and elegant presentations. Indulge in sweet perfection with us.",
+    image_url: "sweetsdesserts.jpg"
+  },
+  {
+    title: "Floral Arrangements",
+    description: "Beautify your event with our stunning floral arrangements. Enjoy fresh, vibrant flowers and creative designs. Elevate your occasion with our floral expertise.",
+    image_url: "floralarrangements.jpg"
+  },
+  {
+    title: "Catering Services",
+    description: "Savor the finest flavors with our catering services. Enjoy gourmet cuisine and exceptional service. Make your event a culinary delight with us.",
+    image_url: "cateringservices.jpg"
+  },
+  {
+    title: "Photography",
+    description: "Capture every moment with our professional photography. Enjoy high-quality images and creative compositions. Preserve your memories beautifully with us.",
+    image_url: "photography.jpg"
+  },
+  {
+    title: "Invitation Design",
+    description: "Set the tone with our custom invitation design. Enjoy elegant and unique invitations tailored to your event. Make a lasting impression from the start.",
+    image_url: "invitationdesign.jpg"
+  },
+  {
+    title: "Cake Design",
+    description: "Indulge in our exquisite wedding cake design. Enjoy beautiful, delicious cakes crafted to perfection. Celebrate your special day with a stunning centerpiece.",
+    image_url: "cakedesign.jpg"
+  },
+  {
+    title: "Decor Rentals",
+    description: "Enhance your event with our premium decor rentals. Enjoy a wide selection of stylish furnishings and accessories. Create the perfect ambiance with us.",
+    image_url: "decorrentals.png"
+  }
+]
+
+services.each do |service|
+  Service.create(service)
+end
