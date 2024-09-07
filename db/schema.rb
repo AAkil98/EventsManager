686209f0c6fe068731e6f288944934297ffd6555
Wedade Jerjoub <wedadejerjoub@gmail.com>
2024-09-07 10:59:28 +0100

    shema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_05_141831) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_04_124921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_05_141831) do
 
   create_table "services", force: :cascade do |t|
     t.string "title"
+    t.string "description"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
